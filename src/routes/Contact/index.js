db8402af3e93e7785c0a166836c25a27ea7ebef8
LightJob <byboygringo@gmail.com>
2020-10-04 20:13:12 -0300

    agrego loading al botton de enviar mensaje

--- src/routes/Contact/index.js
+++ src/routes/Contact/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Form , Input, Button} from "antd";
+import React, { useState } from "react";
+import { Form, Input, Button, notification } from "antd";
 import axios from "util/Api";
 const layout = {
   labelCol: { span: 8 },
@@ -14,11 +14,25 @@ const validateMessages = {
 };
 
 const Contact = () => {
-  const onFinish =async (values) => {
+  const [loading, setLoading] = useState(false);
+  const onFinish = async (values) => {
+    setLoading(true);
     try {
-        await axios.post("/contactForm",values);
+      await axios.post("/contactForm", values);
+      notification["success"]({
+        message: 'Su mensaje fue enviado con exito',
+        description:
+          'Su mensaje fue enviado a nuestros servidores en breve nos estaremos conmunicando con mas informacion.',
+      });
+      setLoading(false);
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      notification["error"]({
+        message: 'Hubo un error al enviar su mensaje por favor reintentelo mas tarde',
+        description:
+          'Su mensaje no pude ser enviado correctamente.',
+      });
+      setLoading(false);
     }
   };
   return (
@@ -31,29 +45,22 @@ const Contact = () => {
     >
       <Form.Item
         name={["name"]}
-        label="Nombre y apellido"    
+        label="Nombre y apellido"
         rules={[{ required: true }]}
       >
         <Input />
       </Form.Item>
-      <Form.Item
-        name={ ["email"]}
-        label="Email"
-        rules={[{ type: "email" }]}
-      >
+      <Form.Item name={["email"]} label="Email" rules={[{ type: "email" }]}>
         <Input />
       </Form.Item>
-      <Form.Item
-        name={ ["contact_number "]}
-        label="Numero de contacto"
-      >
+      <Form.Item name={["contact_number "]} label="Numero de contacto">
         <Input />
       </Form.Item>
       <Form.Item name={["message"]} label="Mensaje">
         <Input.TextArea />
       </Form.Item>
-      <Form.Item className="btnEnviar" style={{width:"8rem"}}>
-        <Button type="primary" style={{width:"8rem"}} htmlType="submit">
+      <Form.Item className="btnEnviar" style={{ width: "8rem" }}>
+        <Button loading={loading} type="primary" style={{ width: "8rem" }} htmlType="submit">
           Enviar
         </Button>
       </Form.Item>
